0ef40df52367d4c1f06ab55c6fddab707cda7c6e
SARA ZIADA <sziada276@gmail.com>
2022-01-23 23:06:24 +0200

    navbar plus logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import Admin from './Components/Admin';
+import NavBar from './Components/NavBar';
+// import NavBar from './Components/Login';
+import './styles/App.css';
 
 export class App extends Component {
   render() {
     return <div>
+      <NavBar />
+      {/* <Login /> */}
      <Admin />
     </div>;
   }
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import '../styles/navBar.css';
+
 class NavBar extends Component {
     render(){
         return(
-
+            <div class="navBar-Container">
+                <img width="100" height="80" src="elevation.png" class="attachment-full size-full" alt="" />
+                <h2>Elevation</h2>
+                <div class="logoButtons">
+                    <button>Profile</button>
+                    <button>Logout</button>
+                </div> 
+            </div>
         );
     }
 }
