fb68ed66757059a0444a70ef1e9c12121052e6d5
fadiad <fadi.1997.id@gmail.com>
2022-01-23 16:50:33 +0200

    commmiting mongoose

--- server/models/job.js
+++ server/models/job.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+// const mongoose = require('mongoose')
+// const Schema = mongoose.Schema
 
-const jobSchema = new Schema({
-    CompanyName: String,
-    JobTitle: String,
-    Location: String,
-    isActive: {
-        type: Boolean,
-        default: true
-    },
-    interviews: [{ type: Schema.Types.ObjectId, ref: 'interview' }]
-})
+// const jobSchema = new Schema({
+//     CompanyName: String,
+//     JobTitle: String,
+//     Location: String,
+//     isActive: {
+//         type: Boolean,
+//         default: true
+//     },
+//     interviews: [{ type: Schema.Types.ObjectId, ref: 'interview' }]
+// })
 
-const Job = mongoose.model("job", jobSchema)
-module.exports = Job
+// const Job = mongoose.model("job", jobSchema)
+// module.exports = Job
--- server/models/mocData.js
+++ server/models/mocData.js
@@ -1,3 +1,3 @@
-const users = []
-const processes = []
-const interviews = []
+// const users = []
+// const processes = []
+// const interviews = []
