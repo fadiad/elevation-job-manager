37868382ebe623a67f27a3f5076330934ec0261f
fadiad <fadi.1997.id@gmail.com>
2022-01-24 14:21:06 +0200

    adding typescript to the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-import Admin from './Components/Admin';
+import Admin from './Components/Admin/Admin';
 import NavBar from './Components/NavBar';
 // import NavBar from './Components/Login';
 import './styles/App.css';
 
 export class App extends Component {
+
+  deleteUser(x:number) {
+ 
+  }
+
+
   render() {
     return <div>
       <NavBar />
