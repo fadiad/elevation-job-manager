7e8ac1b33e42ec8405d967e7e9633f1a8b2d3e29
SARA ZIADA <sziada276@gmail.com>
2022-01-26 11:26:29 +0200

    add admin to admin table

--- server/models/data.sql
+++ server/models/data.sql
@@ -2,8 +2,9 @@ USE jobManagerDB;
 
 INSERT INTO userproporties VALUES(NULL,"Sara","Ziada","sziada276@gmail.com","050-7339214","1234");
 INSERT INTO Candidate VALUES(1,'Student',false,'Atidna 1',NULL);
--- INSERT INTO Admin VALUES(1,true);
--- DELETE FROM Admin;
+
+DELETE FROM Admin;
+
 INSERT INTO Process (id,companyName,jobTitle,location,foundBy,link,UserId)
 VALUES(NULL,"Intel","Full Stack Developer","Haifa",'linkedIn',"h://..",1);
 
@@ -15,5 +16,10 @@ VALUES('Phone',"Avi","Passed",1);
 
 INSERT INTO Interview(type,interviewerName,status,processId)
 VALUES('HR',"Avi","Pending",1);
+
+USE jobManagerDB;
+
+INSERT INTO userproporties VALUES(NULL,Null,"Ameer","","admin@gmail.com","","1234",true);
+INSERT INTO Admin VALUES(2,true);
 
 
