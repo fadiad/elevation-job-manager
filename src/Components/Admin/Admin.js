442ef951e3edfe4768f05302d03724168adbc57c
SARA ZIADA <sziada276@gmail.com>
2022-02-01 13:00:37 +0200

    Statistics UI

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -5,7 +5,7 @@ import GeneralStatistics from './GeneralStatistics'
 import StatisticsByFilter from './StatisticsByFilter'
 import Filter from './Filter'
 import NavBar from '../NavBar';
-import PieChartByFilter from './PieChartByFilter'
+import '../../styles/Admin.css'
 
 class Admin extends Component {
 
@@ -19,8 +19,11 @@ class Admin extends Component {
                 <NavBar/>
                 <Filter />
                 adminName : {this.props.adminStore.adminName}
-                <GeneralStatistics/>
-                <StatisticsByFilter/>
+                <div className='all-statistics'>
+                    <GeneralStatistics/>
+                    <StatisticsByFilter/>
+                </div>
+                
                 {/* <PieChartByFilter/> */}
                 <UsersInterviews />
 
